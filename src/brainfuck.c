567f0703d056cb86e7b0aacc9b6b8f21d61ddc3e
Olle K <olle.kvarnstroem@gmail.com>
2014-05-13 14:20:42 +0200

    Fixed bug with nestled brackets

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -40,12 +40,14 @@ void free_brainfuck_stack() {
 
 static int _brainfuck(char *line) {
   char *c = line;
-  char *tmpline = NULL;
   if (c == NULL) {
     return 0;
   }
 
-  while (*c) {
+  while (*c != '\0') {
+    size_t sublen;
+    long num_brackets;
+
     switch (*c++) {
       case '.': putchar(stack[stack_ptr]); break;
       case ',': stack[stack_ptr] = getchar(); break;
@@ -56,32 +58,43 @@ static int _brainfuck(char *line) {
       case '?': printf("@%d[%d]\n", stack_ptr, stack[stack_ptr]); break;
 
       case '[':
-        /* Make sure there's a closing bracket before executing */
-        if (strchr(c, ']') == NULL) {
-          fprintf(stderr, "Warning: No closing bracket found. "
-                          "The following code will not be executed:\n%s\n",
-                          &c[-1]);
-          return 1;
+        /* Find the matching closing bracket */
+        sublen = 0;
+        num_brackets = 1;
+        for (char *sub = c; *sub != '\0'; ++sub, ++sublen) {
+          if      (*sub == '[') { ++num_brackets; }
+          else if (*sub == ']') { --num_brackets; }
+          if (num_brackets == 0) {
+            break;
+          }
         }
 
-        if ((tmpline = malloc(sizeof *tmpline * (strlen(c) +1))) == NULL) {
-          fprintf(stderr, "Virtual memory exceeded!\n");
-          abort();
+        if (stack[stack_ptr] != 0) {
+          char *tmpline;
+
+          /* Create a subproc */
+          if ((tmpline = malloc(sizeof *tmpline * (sublen +1))) == NULL) {
+            fprintf(stderr, "Virtual memory exceeded!\n");
+            return 1;
+          }
+          strncpy(tmpline, c, sublen +1);
+          tmpline[sublen] = '\0';
+
+          /* Execute subproc until pointer points to 0 */
+          while (stack[stack_ptr] != 0) {
+            _brainfuck(tmpline);
+          }
+
+          /* Remove subproc */
+          free(tmpline);
+          tmpline = NULL;
         }
-        strcpy(tmpline, c);
-        _brainfuck(tmpline);
-        free(tmpline);
-        tmpline = NULL;
-        for (; *c != ']' && *c != '\0'; ++c);
-        for (; *c == ']'; ++c);
+
+        /* Afterwards, move to end of subproc */
+        c += sublen +1;
         break;
 
-      case ']':
-        if (stack[stack_ptr] != 0) {
-          c = line;
-        } else {
-          return 0;
-        } break;
+      case ']': return 0; break;
     }
   }
   return 0;
