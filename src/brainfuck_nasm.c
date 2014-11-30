7ec3ce0cb856575cb155060ef5b3f0708f38d1dd
Olle Kvarnström <olle+git@iix.se>
2014-11-30 12:22:54 +0100

    add comments

--- src/brainfuck_nasm.c
+++ src/brainfuck_nasm.c
@@ -30,7 +30,7 @@ static inline void op_mvl(FILE *output) {
 static void op_tag(FILE *output) {
   fprintf(output, "\tcmp\tbyte[ebp],0\n"
                   "\tjz\tend%d%d\n"
-                  "tag%d%d:\n",
+                  "tag%d%d:",
           ptr, stack[ptr], ptr, stack[ptr]);
   ++ptr;
 }
@@ -38,7 +38,7 @@ static void op_tag(FILE *output) {
 static void op_jmp(FILE *output) {
   fprintf(output, "\tcmp\tbyte[ebp],0\n"
                   "\tjnz\ttag%d%d\n"
-                  "end%d%d:\n",
+                  "end%d%d:",
                   ptr -1, stack[ptr -1], ptr -1, stack[ptr -1]);
   stack[--ptr]++;
 }
@@ -46,23 +46,22 @@ static void op_jmp(FILE *output) {
 int brainfuck_nasm_write(FILE *output, FILE *input) {
   fprintf(output, "section .text\n"
                   "\tglobal _start\n"
-  /* PUT */       "_put:\n"
-                  "\tmov\teax,4\n"
-                  "\tmov\tebx,1\n"
+  /* PUT */       "_put:\t\t\t\t; sys_write(stdout, ebp, 1)\n"
+                  "\tmov\teax,4\t\t; sys_write\n"
+                  "\tmov\tebx,1\t\t; stdout\n"
                   "\tmov\tecx,ebp\n"
-                  "\tmov\tedx,1\n"
                   "\tint\t80h\n"
                   "\tret\n"
-  /* GET */       "_get:\n"
-                  "\tmov\teax,3\n"
-                  "\tmov\tebx,0\n"
+  /* GET */       "_get:\t\t\t\t; sys_read(stdin, ebp, 1)\n"
+                  "\tmov\teax,3\t\t; sys_read\n"
+                  "\tmov\tebx,0\t\t; stdin\n"
                   "\tmov\tecx,ebp\n"
-                  "\tmov\tedx,1\n"
                   "\tint\t80h\n"
                   "\tret\n"
   /* START */     "_start:\n"
-                  "\tsub\tesp,7000\n"
-                  "\tmov\tebp,esp\n");
+                  "\tsub\tesp,7000\t; 7000 byte stack\n"
+                  "\tmov\tebp,esp\t\t; Set ptr to start of stack\n"
+                  "\tmov\tedx,1\t\t; edx (for _put and _get) is always 1\n");
 
   int op;
   while ((op = fgetc(input)) != EOF) {
@@ -78,7 +77,7 @@ int brainfuck_nasm_write(FILE *output, FILE *input) {
     }
   }
 
-  fprintf(output, "\tmov\teax,1\n"
+  fprintf(output, "\tmov\teax,1\t\t; sys_exit(0)\n"
                   "\tmov\tebx,0\n"
                   "\tint\t80h\n");
   return 0;
